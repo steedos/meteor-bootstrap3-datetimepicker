c23c711e00bb490a54f6470540124f02259bce8a
yinlianghui <yinlianghui@hotoa.com>
2018-05-04 17:57:18 +0800

    源码升级到4.17.47

--- package.js
+++ package.js
@@ -1,18 +1,18 @@
 Package.describe({
-  name: 'tsega:bootstrap3-datetimepicker',
-  summary: "Bootstrap 3 DateTime picker from @Eonasdan, packaged for Meteor.js",
-  version: "4.17.42",
-  git: "https://github.com/tsega/meteor-bootstrap3-datetimepicker"
+	name: 'steedos:bootstrap3-datetimepicker',
+	summary: "Bootstrap 3 DateTime picker from @Eonasdan, packaged for Meteor.js",
+	version: "4.17.47",
+	git: "https://github.com/tsega/meteor-bootstrap3-datetimepicker"
 });
 
 Package.onUse(function (api) {
-    api.versionsFrom('METEOR@1.2');
+	api.versionsFrom('METEOR@1.2');
 
-    api.use('jquery');
-    api.use('momentjs:moment@2.13.1');
+	api.use('jquery');
+	api.use('momentjs:moment@2.13.1');
 
-    api.addFiles([
-      'lib/css/bootstrap-datetimepicker.css', // Add Date Time Picker CSS and JS files
-      'lib/js/bootstrap-datetimepicker.js',
-    ], 'client');
+	api.addFiles([
+		'lib/css/bootstrap-datetimepicker.css', // Add Date Time Picker CSS and JS files
+		'lib/js/bootstrap-datetimepicker.js',
+	], 'client');
 });
